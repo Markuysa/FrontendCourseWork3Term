3de9dec01079ca8c1270ed1b4148abf8182f734d
Markuysa <islam.kus0101@gmail.com>
2022-11-14 00:28:58 +0300

    adaptrive ( menu / meditationPage + modal)

--- src/components/pageTemplates/asidebar.jsx
+++ src/components/pageTemplates/asidebar.jsx
@@ -1,27 +1,32 @@
+import { useEffect, useState } from "react";
 import "../../css/asideBar.css"
 
 const AsideBar = ({items})=>{
-    console.log(items)
-    let listItem = document.querySelectorAll(".asideBarItem");
-    for (let i=0;i<listItem.length;i++){
-        listItem[i].onClick = ()=>{
-            let j = 0;
-            while (j<listItem.length){
-                listItem[j++].className = 'asideBarItem'
+    let listItem
+    useEffect(()=>{
+        listItem = document.querySelectorAll(".asideBarItem");
+    })
+    const onAsideClick = (index)=>{
+        listItem[index].classList.add("active")
+        for (let i=0;i<listItem.length;i++){
+            
+            if (listItem[i].classList.contains("active")){
+                listItem[i].classList.remove("active")
+                listItem[i].style.backgroundColor = "white"
+                listItem[i].style.color = "black"
             }
-            listItem[i].classList.add("active")
-            alert(listItem[i].classList)
-        }
+        } 
     }
+    const [selectedFilter,setSelectedFilter]  = useState('');
     return (
         <div className="asideBarItems">
             <ul>
                 {/* // items.map(item=>{
                 //     <li className="asideBarItem">{item.title}</li>
                 // })    */}
-                <li className="asideBarItem"  >To sleep</li>
-                <li className="asideBarItem" >To study</li>
-                <li className="asideBarItem" >To relax</li>
+                <li className="asideBarItem" onClick={onAsideClick} >To sleep</li>
+                <li className="asideBarItem" onClick={onAsideClick}>To study</li>
+                <li className="asideBarItem" onClick={onAsideClick}>To relax</li>
             </ul>
         </div>
     );
